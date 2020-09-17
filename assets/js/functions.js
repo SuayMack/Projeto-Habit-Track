9acbf6f9e31dba42a0190109e998821a2d68c187
PriscilaCS <39917483+SuayMack@users.noreply.github.com>
2020-09-17 20:53:08 -0300

    função js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,20 @@
-function myFunction() {
-  var color = document.getElementById("color").innerHTML = this.getElementById();
-  return color
+addEventListenerAll(element, events, fn){
+  events.split(' ').forEach(event => {
+    element.addEventListener(event, fn, false)
+  })
+}
 
+
+
+function myFunction(){   
+  let buttons = document.querySelectorAll("#buttons > button")
+    //percorre cada botão
+    buttons.forEach((btn, index)=>{
+      this.addEventListenerAll(btn, "click drag mouseover", e=>{
+        console.log(btn.id)
+      })
+  })
+
+  
+  
 }
